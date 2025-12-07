7f9b40d14124be4e9c923520c3812a9d2a258935
f0r3s1 <151480120+f0r3s1@users.noreply.github.com>
2025-12-07 20:30:25 +0000

    fix: autoplay video preview in SceneCard component

--- ui/src/views/scenes/SceneCard.vue
+++ ui/src/views/scenes/SceneCard.vue
@@ -6,7 +6,7 @@
            @click="showDetails(item)"
            @mouseover="preview = true"
            @mouseleave="preview = false">
-        <video v-if="preview && item.has_preview" :src="`/api/dms/preview/${item.scene_id}`" autoplay loop></video>
+        <video v-if="preview && item.has_preview" :src="`/api/dms/preview/${item.scene_id}`" autoplay loop muted></video>
         <div class="overlay align-bottom-left">
           <div style="padding: 5px">
             <b-tag v-if="item.is_watched && !this.$store.state.optionsWeb.web.sceneWatched">
